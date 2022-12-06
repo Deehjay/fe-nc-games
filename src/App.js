8bcc57cee198be0530b7e2fc2db4def0cf289896
Daniel James <danielcjamesdj@gmail.com>
2022-12-06 11:50:04 +0000

    Implement SingleReview component into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Reviews from "./components/Reviews";
+import SingleReview from "./components/SingleReview";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Reviews />
+      <Routes>
+        <Route path="/" element={<Reviews />} />
+        <Route path="/reviews/:review_id" element={<SingleReview />} />
+      </Routes>
       <Footer />
     </div>
   );
