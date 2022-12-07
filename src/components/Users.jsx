4c44d4aada4456080cfd7527ae0a986f435e30e7
Daniel James <danielcjamesdj@gmail.com>
2022-12-07 10:21:00 +0000

    Alter Users component to use isLoggedIn context

---
+++ src/components/Users.jsx
@@ -0,0 +1,51 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getUsers } from "../api";
+import { UserContext } from "../contexts/users";
+
+const Users = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const { setUser } = useContext(UserContext);
+  const navigateHome = useNavigate();
+  const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
+
+  useEffect(() => {
+    getUsers().then((users) => {
+      setUsers(users);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const handleSignIn = (user) => {
+    setUser(user);
+    setIsLoggedIn(true);
+    navigateHome("/");
+  };
+
+  return isLoading ? (
+    <div className="loader"></div>
+  ) : (
+    <main className="users-main">
+      <h2>Users List</h2>
+      <ul className="users-list">
+        {users.map((user) => {
+          return (
+            <li className="user-card" key={user.username}>
+              <img src={user.avatar_url} alt={user.username} />
+              <h3>{user.username}</h3>
+              <button
+                onClick={() => {
+                  handleSignIn(user);
+                }}>
+                Select user
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+};
+
+export default Users;
