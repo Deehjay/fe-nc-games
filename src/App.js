b01ac9bd861261e0b3aea9e13846df151a0003b9
Daniel James <danielcjamesdj@gmail.com>
2022-12-07 10:18:36 +0000

    Remove isLoggedIn state from App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useContext, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Reviews from "./components/Reviews";
 import SingleReview from "./components/SingleReview";
+import Users from "./components/Users";
+import { UserContext } from "./contexts/users";
 
 function App() {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="App">
       <Nav />
       <Routes>
         <Route path="/" element={<Reviews />} />
         <Route path="/reviews/:review_id" element={<SingleReview />} />
+        <Route path="/login" element={<Users />} />
       </Routes>
       <Footer />
     </div>
