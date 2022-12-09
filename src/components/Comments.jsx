76e056218f3b806f2efd0f4b933cc10d7c1abc06
Daniel James <danielcjamesdj@gmail.com>
2022-12-09 10:55:54 +0000

    Remove optimistic rendering for comments, implement delete comment functionality

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getCommentsByReviewId, postComment } from "../api";
+import { deleteComment, getCommentsByReviewId, postComment } from "../api";
 import { formatDate } from "../utils/utils";
 import { BiLike } from "react-icons/bi";
 import Collapsible from "react-collapsible";
 import { UserContext } from "../contexts/users";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 const Comments = ({ review }) => {
   const [comments, setComments] = useState([]);
@@ -13,6 +15,7 @@ const Comments = ({ review }) => {
   const [commentBody, setCommentBody] = useState("");
   const { user, isLoggedIn } = useContext(UserContext);
   const [loginPrompt, setLoginPrompt] = useState(null);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -33,24 +36,47 @@ const Comments = ({ review }) => {
     e.preventDefault();
     if (isLoggedIn && commentBody) {
       setCommentBody("");
-      setComments((currComments) => {
-        return [
-          {
-            author: user.username,
-            body: commentBody,
-            created_at: new Date(),
-            votes: 0,
-          },
-          ...currComments,
-        ];
-      });
-      postComment(review_id, user.username, commentBody);
+      postComment(review_id, user.username, commentBody).then(
+        (postedComment) => {
+          setComments((currComments) => {
+            return [postedComment, ...currComments];
+          });
+        }
+      );
     } else if (!commentBody) {
       setLoginPrompt("Comment body must not be empty!");
     } else {
       setCommentBody("");
       setLoginPrompt("Please log in to comment");
     }
+  };
+
+  const handleDeleteComment = (comment_id) => {
+    setIsDeleting(true);
+    deleteComment(comment_id).then(() => {
+      setIsDeleting(false);
+      const updatedComments = comments.filter((comment) => {
+        return comment.comment_id !== comment_id;
+      });
+      setComments(updatedComments);
+    });
+  };
+
+  const handleDeleteButton = (comment_id) => {
+    confirmAlert({
+      title: "Delete comment",
+      message: "Are you sure to do delete your comment?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => handleDeleteComment(comment_id),
+        },
+        {
+          label: "No",
+          onClick: null,
+        },
+      ],
+    });
   };
 
   return isLoading ? (
@@ -88,6 +114,17 @@ const Comments = ({ review }) => {
                   <span>
                     <BiLike /> {comment.votes}
                   </span>
+                  {user.username === comment.author ? (
+                    <button
+                      onClick={() => {
+                        handleDeleteButton(comment.comment_id);
+                      }}>
+                      Delete
+                    </button>
+                  ) : null}
+                  {isDeleting && user.username === comment.author ? (
+                    <p>Deleting comment...</p>
+                  ) : null}
                 </li>
               );
             })}
