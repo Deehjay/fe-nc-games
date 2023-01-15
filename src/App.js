8bd76b18a6cab27c0f131f0113942fa397a10969
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-15 21:23:16 +0000

    Import semantic reac css into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ErrorPage from "./components/ErrorPage";
 import Nav from "./components/Nav";
+import PostReview from "./components/PostReview";
 import Reviews from "./components/Reviews";
 import SingleReview from "./components/SingleReview";
 import Users from "./components/Users";
@@ -14,6 +16,8 @@ function App() {
         <Route path="/categories/:category_slug" element={<Reviews />} />
         <Route path="/reviews/:review_id" element={<SingleReview />} />
         <Route path="/login" element={<Users />} />
+        <Route path="/postreview" element={<PostReview />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       {/* <Footer /> */}
     </div>
