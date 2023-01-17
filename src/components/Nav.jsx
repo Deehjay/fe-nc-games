a3594c3dd9433d170f980123fb2359857f059e24
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-17 22:41:21 +0000

    Alter Nav to use separate components for mobile and desktop views

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
-import React, { useContext, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/users";
+import DesktopNav from "./DesktopNav";
+import MobileNav from "./MobileNav";
 
 const Nav = () => {
   const { user, setUser, isLoggedIn, setIsLoggedIn } = useContext(UserContext);
-  const navigate = useNavigate();
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 620;
 
   useEffect(() => {
     const loggedInUser = localStorage.getItem("user");
@@ -15,32 +17,17 @@ const Nav = () => {
     }
   }, []);
 
-  const handleLogout = () => {
-    setUser({ username: "Guest" });
-    setIsLoggedIn(false);
-    localStorage.clear();
-    navigate("/");
-  };
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
 
-  return (
-    <nav className="nav">
-      <Link to="/" id="logo">
-        HOUSE OF GAMES
-      </Link>
-      {isLoggedIn ? (
-        <div className="logged-in">
-          <Link to="/my-profile">
-            <img src={user.avatar_url} alt={user.username} />
-          </Link>
-          <Link to="/post-review">+</Link>
-          <Link to="/" onClick={handleLogout}>
-            Logout
-          </Link>
-        </div>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
-    </nav>
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  return width < breakpoint ? (
+    <MobileNav isLoggedIn={isLoggedIn} user={user} />
+  ) : (
+    <DesktopNav isLoggedIn={isLoggedIn} user={user} />
   );
 };
 
