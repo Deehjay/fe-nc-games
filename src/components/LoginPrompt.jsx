e00ca24ba3f3eecd166c09920eb97ea5eaa3bbd7
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-17 13:29:08 +0000

    Move LoginPrompt to be inside of a h2

--- src/components/LoginPrompt.jsx
+++ src/components/LoginPrompt.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LoginPrompt = () => {
-  return <div>Please log in to view this page</div>;
+  return (
+    <div>
+      <h2>Please log in to view this page</h2>
+    </div>
+  );
 };
 
 export default LoginPrompt;
